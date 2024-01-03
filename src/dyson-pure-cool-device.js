a9096b3bd09113d03108f2d01028f948ab874e95
Eric Jacobs <ejacobs@doximity.com>
2024-01-03 12:29:54 -0700

    Adds auto mode switch
    Implemented exactly like the night mode switch, the auto mode switch
    allows toggling auto mode without changing any other settings.
    
    My use case is that I like to run the fan at high speed at night and
    then switch it to auto-mode during the day. Currently, I'm not able to
    automate the transition into auto-mode because setting the
    characteristics with a scene also sets the fan speed. With the auto mode
    switch, I'll be able to just toggle auto mode to on in my morning
    automation.

--- src/dyson-pure-cool-device.js
+++ src/dyson-pure-cool-device.js
@@ -322,6 +322,15 @@ function DysonPureCoolDevice(platform, name, serialNumber, productType, version,
         }
     }
 
+    // Updates the auto mode
+    let autoModeSwitchService = null;
+    if (switchAccessory && config.isAutoModeEnabled) {
+        autoModeSwitchService = switchAccessory.getServiceByUUIDAndSubType(Service.Switch, 'AutoMode');
+        if (!autoModeSwitchService) {
+            autoModeSwitchService = switchAccessory.addService(Service.Switch, device.info.name + ' Auto Mode', 'AutoMode');
+        }
+    }
+
     // Updates the jet focus mode
     let jetFocusSwitchService = null;
     if (switchAccessory && config.isJetFocusEnabled && device.info.hasJetFocus) {
@@ -604,6 +613,11 @@ function DysonPureCoolDevice(platform, name, serialNumber, productType, version,
                 nightModeSwitchService.updateCharacteristic(Characteristic.On, content['product-state']['nmod'] !== 'OFF');
             }
 
+            // Sets the state of the auto mode switch
+            if (autoModeSwitchService) {
+                autoModeSwitchService.updateCharacteristic(Characteristic.On, content['product-state']['auto'] !== 'OFF');
+            }
+
             // Sets the state of the jet focus switch
             if (jetFocusSwitchService) {
                 if (content['product-state']['fdir']) {
@@ -697,6 +711,11 @@ function DysonPureCoolDevice(platform, name, serialNumber, productType, version,
             // Sets the state of the night mode switch
             if (nightModeSwitchService) {
                 nightModeSwitchService.updateCharacteristic(Characteristic.On, content['product-state']['nmod'][1] !== 'OFF');
+            }
+
+            // Sets the state of the auto mode switch
+            if (autoModeSwitchService) {
+                autoModeSwitchService.updateCharacteristic(Characteristic.On, content['product-state']['auto'][1] !== 'OFF');
             }
 
             // Sets the state of the jet focus switch
@@ -901,6 +920,33 @@ function DysonPureCoolDevice(platform, name, serialNumber, productType, version,
 
             // Sends the command
             platform.log.info(serialNumber + ' - set NightMode to ' + value + ': ' + JSON.stringify(commandData));
+            device.mqttClient.publish(productType + '/' + serialNumber + '/command', JSON.stringify({
+                msg: 'STATE-SET',
+                time: new Date().toISOString(),
+                data: commandData
+            }));
+            callback(null);
+        });
+    }
+
+    // Subscribes for changes of the auto mode
+    if (autoModeSwitchService) {
+        autoModeSwitchService.getCharacteristic(Characteristic.On).on('set', function (value, callback) {
+
+            // Builds the command data, if auto mode is set to ON, the device has to be ON
+            // If auto mode is set to OFF, the device status is not changed
+            let commandData = {};
+            if (value) {
+                commandData = { auto: 'ON', fmod: 'AUTO' }
+                if (!airPurifierService.getCharacteristic(Characteristic.Active).value) {
+                    commandData['fpwr'] = 'ON'
+                }
+            } else {
+                commandData = { auto: 'OFF', fmod: 'FAN' };
+            }
+
+            // Sends the command
+            platform.log.info(serialNumber + ' - set AutoMode to ' + value + ': ' + JSON.stringify(commandData));
             device.mqttClient.publish(productType + '/' + serialNumber + '/command', JSON.stringify({
                 msg: 'STATE-SET',
                 time: new Date().toISOString(),
